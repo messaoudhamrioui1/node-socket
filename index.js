fcb245be28032ddf562ad7f8ff544238a94436d0
messaoudhamrioui1 <messaoud.hamrioui@gmail.com>
2022-05-26 21:49:35 +0200

    1 file modified

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(ip); // ip address of the user
-
   var geo = geoip.lookup(ip);
-  console.log(geo);
+  console.log('connection from :', geo);
 });
 
 app.use('/', router);
@@ -24,11 +22,9 @@ io.on('connection', (socket) => {
   // const address = socket.request.connection.remoteAddress;
   const ip = socket.request.headers['x-forwarded-for'];
   const geo = geoip.lookup(ip);
-  console.log(geo);
   socket.on('message', (message) => {
     console.log(message);
     io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
-    io.emit('localisation', geo);
   });
 
   socket.on('disconnect', () => {
