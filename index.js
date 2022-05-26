a94a3fd4e0e919ebb13a62493b29961544cbb28d
messaoudhamrioui1 <messaoud.hamrioui@gmail.com>
2022-05-26 17:50:36 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('express')();
+const express = require('express');
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer, {
   cors: { origin: '*' },
