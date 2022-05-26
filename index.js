de76ac8140c5a708cf8c08fc075ceb3bfd9c97e5
messaoudhamrioui1 <messaoud.hamrioui@gmail.com>
2022-05-26 21:43:14 +0200

    1 file modified

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ io.on('connection', (socket) => {
   socket.on('message', (message) => {
     console.log(message);
     io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
+    io.emit('localisation', geo);
   });
 
   socket.on('disconnect', () => {
